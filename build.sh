2a7438bbc21e20133603e1d6d668596d41bafc86
golgxyz <664666105@mail.pbru.ac.th>
2026-05-21 12:29:13 +0700

    Fix build.sh: install whitenoise explicitly

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install --upgrade pip
+pip install whitenoise gunicorn dj-database-url psycopg2-binary
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
